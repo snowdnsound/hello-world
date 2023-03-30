c44bf0356fc56f702b811b7007b2216bed4a3677
Justin Benjamin <justinjbenjamin@gmail.com>
2023-03-30 10:39:01 -0700

    Make user give us the right thingies

--- app.js
+++ app.js
@@ -21,14 +21,31 @@ function catChecker(){
     document.write("We love " + usersCat + " and string conCATenation. ");
 }
 
+function guessCats(){
+    let myNumber = 2;
+    let usersGuess = prompt("Guess how many cats do I have between 1 & 6?");
+    while (usersGuess != myNumber){
+    usersGuess = prompt("Wrong! Guess again, a number between 1 & 6");
+  }
+  let catMessage = "Yes I have two cats!";
+  alert(catMessage);
+}
+
 function ageChecker(){
     let age = prompt("How old are you?");
-    if (age > 21) {
+    if (age > 20) {
     document.write("Welome to adulthood, now scoop my litterbox.");
     } else {
     window.location.href = "https://google.com";
 }
 }
 
+let userNum = prompt("How many starts you would give this experience 1-5?");
+for(let i = 0; i < userNum; i++){
+    document.write("<img src='images/Star.png' alt='orange stars'/>");
+}
+
+
 catChecker();
+guessCats();
 ageChecker();
